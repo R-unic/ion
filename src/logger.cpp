fa29563c227ff66f30d0f19d891638aaaf80bdf9
R-unic <roleyspersonal@gmail.com>
2025-10-02 15:27:32 -0400

    feat: logger colors

--- src/logger.cpp
+++ src/logger.cpp
@@ -2,48 +2,37 @@
 #include <iostream>
 
 #include "ion/logger.h"
-
-static std::tm get_local_time()
-{
-    const auto now = std::chrono::system_clock::now();
-    const auto now_time = std::chrono::system_clock::to_time_t(now);
-    std::tm local_tm {};
-#ifdef _WIN32
-    localtime_s(&local_tm, &now_time); // Windows secure version
-#else
-    localtime_r(&now_time, &local_tm); // POSIX thread-safe version
-#endif
-
-    return local_tm;
-}
+#include "ion/utility/basic.h"
 
 static void log(const std::string& tag, const std::string& message)
 {
-    const auto local_time = get_local_time();
-    std::cout << "[" << std::put_time(&local_time, "%H:%M:%S") << " " << tag << "] " << message << '\n';
+    using namespace std::chrono;
+
+    const auto time_stamp = color(std::format("{:%H:%M:%S}", floor<seconds>(system_clock::now())), Color::gray);
+    std::cout << "[" << time_stamp << " " << tag << "] " << message << '\n';
 }
 
 namespace logger
 {
     void info(const std::string& message)
     {
-        log("INFO", message);
+        log(color("INFO", Color::light_blue), message);
     }
 
     void warn(const std::string& message)
     {
-        log("WARN", message);
+        log(color("WARN", Color::yellow), message);
     }
 
     [[noreturn]] void error(const std::string& message, const uint8_t code)
     {
-        log("ERR", message);
+        log(color("ERR", Color::red), message);
         exit(code);
     }
 
     void debug(const std::string& message)
     {
-        log("DBG", message);
+        log(color("DBG", Color::purple), message);
     }
 
     void verbose(const std::string& message)
