f51570bccb7449794e6d7e9557e159907589a640
R-unic <roleyspersonal@gmail.com>
2025-10-10 13:31:25 -0400

    feat: is_literal & is_primitive_literal functions

--- src/ast/node.cpp
+++ src/ast/node.cpp
@@ -1,6 +1,21 @@
 #include "ion/diagnostics.h"
 #include "ion/ast/node.h"
 
+Token SyntaxNode::get_last_token() const
+{
+    return get_first_token();
+}
+
+FileSpan SyntaxNode::get_span() const
+{
+    const auto first = get_first_token();
+    const auto last = get_last_token();
+    if (first.span == last.span)
+        return first.span;
+
+    return create_span(first.span.start, last.span.end);
+}
+
 symbol_ptr_t SyntaxNode::get_symbol()
 {
     COMPILER_ASSERT(symbol.has_value(), "No symbol for AST node found");
