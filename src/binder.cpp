1417e3091acdb483b436a8205139d6c5a168561a
R-unic <roleyspersonal@gmail.com>
2025-10-12 18:08:33 -0400

    feat: enum symbols & bind types to explicitly typed symbols

--- src/binder.cpp
+++ src/binder.cpp
@@ -3,6 +3,7 @@
 #include "ion/intrinsics.h"
 #include "ion/symbols/named_symbol.h"
 #include "ion/symbols/type_declaration_symbol.h"
+#include "ion/types/type_name.h"
 
 void Binder::bind_declaration_symbol(NamedDeclaration* named_declaration)
 {
@@ -42,6 +43,12 @@ declaration_symbol_ptr_t Binder::define_type_declaration_symbol(const NamedDecla
     return define_symbol(symbol);
 }
 
+named_symbol_ptr_t Binder::define_type_symbol(const std::string& name, type_ptr_t& type)
+{
+    const auto symbol = std::make_shared<TypeSymbol>(name, std::move(type));
+    return define_symbol(symbol);
+}
+
 named_symbol_ptr_t Binder::define_named_symbol(const std::string& name)
 {
     const auto symbol = std::make_shared<NamedSymbol>(name);
@@ -62,9 +69,20 @@ std::optional<named_symbol_ptr_t> Binder::find_named_symbol(const std::string& n
 {
     for (auto it = scopes_.rbegin(); it != scopes_.rend(); ++it)
         for (const auto& symbol_ptr : *it)
-            if (auto named = std::dynamic_pointer_cast<NamedSymbol>(symbol_ptr))
-                if (named->name == name)
-                    return named;
+            if (auto named_symbol = std::dynamic_pointer_cast<NamedSymbol>(symbol_ptr))
+                if (!named_symbol->is_type_symbol() && !named_symbol->is_type_declaration_symbol() && named_symbol->name == name)
+                    return named_symbol;
+
+    return std::nullopt;
+}
+
+std::optional<named_symbol_ptr_t> Binder::find_type_symbol(const std::string& name) const
+{
+    for (auto it = scopes_.rbegin(); it != scopes_.rend(); ++it)
+        for (const auto& symbol_ptr : *it)
+            if (auto type_symbol = std::dynamic_pointer_cast<TypeSymbol>(symbol_ptr))
+                if (type_symbol->name == name)
+                    return type_symbol;
 
     return std::nullopt;
 }
@@ -78,13 +96,14 @@ void Binder::visit_ast(const std::vector<statement_ptr_t>& statements)
     });
 }
 
+
 void Binder::visit_identifier(Identifier& identifier)
 {
     if (const auto symbol_opt = find_named_symbol(identifier.get_text()); symbol_opt.has_value())
     {
         if (const auto& symbol = *symbol_opt; !symbol->is_declaration_symbol())
             identifier.symbol = symbol;
-        else
+        else if (!symbol->is_type_declaration_symbol())
         {
             const auto named_symbol = define_named_symbol(symbol->name);
             named_symbol->declaring_symbol = symbol;
@@ -101,7 +120,45 @@ void Binder::visit_expression_statement(ExpressionStatement& expression_statemen
     expression_statement.symbol = expression_statement.expression->symbol;
 }
 
-DEFINE_EMPTY_SYMBOL_VISITOR(primitive_literal, PrimitiveLiteral);
+void Binder::visit_variable_declaration(VariableDeclaration& variable_declaration)
+{
+    AstVisitor::visit_variable_declaration(variable_declaration);
+    const auto type = variable_declaration.colon_type.has_value()
+                          ? Type::from(variable_declaration.colon_type.value()->type)
+                          : std::optional<type_ptr_t>(std::nullopt);
+
+    const auto symbol = std::make_shared<DeclarationSymbol>(variable_declaration.name.get_text(), &variable_declaration, type);
+    variable_declaration.symbol = define_symbol(symbol);
+}
+
+void Binder::visit_enum_declaration(EnumDeclaration& enum_declaration)
+{
+    AstVisitor::visit_enum_declaration(enum_declaration);
+    bind_declaration_symbol(&enum_declaration);
+
+    auto enum_type = number_type.as_shared(); // TODO: union of literal types
+    const auto _ = define_type_declaration_symbol(&enum_declaration, enum_type);
+}
+
+void Binder::visit_type_name(TypeNameRef& type_name_ref)
+{
+    if (const auto symbol_opt = find_type_symbol(type_name_ref.get_text()); symbol_opt.has_value())
+    {
+        if (const auto& symbol = *symbol_opt; !symbol->is_type_declaration_symbol())
+            type_name_ref.symbol = symbol;
+        else
+        {
+            type_ptr_t type = std::make_shared<TypeName>(type_name_ref.get_text());
+            const auto named_symbol = define_type_symbol(symbol->name, type);
+            named_symbol->declaring_symbol = symbol;
+            type_name_ref.symbol = named_symbol;
+        }
+    }
+
+    ScopedAstVisitor::visit_type_name(type_name_ref);
+}
+
+DEFINE_EMPTY_SYMBOL_VISITOR(primitive_literal, PrimitiveLiteral)
 DEFINE_EMPTY_SYMBOL_VISITOR(array_literal, ArrayLiteral);
 DEFINE_EMPTY_SYMBOL_VISITOR(tuple_literal, TupleLiteral);
 DEFINE_EMPTY_SYMBOL_VISITOR(range_literal, RangeLiteral);
@@ -122,9 +179,8 @@ DEFINE_EMPTY_SYMBOL_VISITOR(await, Await);
 DEFINE_EMPTY_SYMBOL_VISITOR(name_of, NameOf);
 DEFINE_EMPTY_SYMBOL_VISITOR(type_of, TypeOf);
 
-DEFINE_DECLARATION_VISITOR(variable_declaration, VariableDeclaration);
 DEFINE_SCOPED_DECLARATION_VISITOR(function_declaration, FunctionDeclaration);
 DEFINE_SCOPED_DECLARATION_VISITOR(event_declaration, EventDeclaration);
 DEFINE_SCOPED_DECLARATION_VISITOR(instance_constructor, InstanceConstructor);
 DEFINE_TYPE_DECLARATION_VISITOR(type_declaration, TypeDeclaration, Type::from(type_declaration.type));
-DEFINE_TYPE_DECLARATION_VISITOR(interface_declaration, InterfaceDeclaration, Type::from_interface(interface_declaration));
+DEFINE_TYPE_DECLARATION_VISITOR(interface_declaration, InterfaceDeclaration, Type::from_interface(interface_declaration))
